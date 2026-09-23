d5c18cb2bd8dc640fca4255983614783191273ed
jeanzarricueta <jeanzarricueta9@gmail.com>
2026-09-23 02:18:24 -0300

    commits 4: creacion de la funcion de vista en views.py con envio de contexto

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .dataimport productos
+
+def mostrar_productos(request):
+    context = {
+        'productos': productos
+    }
+
+    return render(request, 'catalogo/index.html', contexto)
 
 # Create your views here.
