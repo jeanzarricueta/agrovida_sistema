4728d8e3f8c7ca5caa587c2f3d31cfea6d1ab20a
jeanzarricueta <jeanzarricueta9@gmail.com>
2026-09-23 02:47:55 -0300

    commits 5 : creacion del templates y visualizacion del objeto de datos en el navegador

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .dataimport productos
+from .data import productos
 
 def mostrar_productos(request):
     context = {
         'productos': productos
     }
 
-    return render(request, 'catalogo/index.html', contexto)
+    return render(request, 'catalogo/index.html', context)
 
 # Create your views here.
